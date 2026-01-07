2b92dc4aa5f27cfa74bbe6d7549c37991dcd72e8
blai <bmunoz@hesstec.net>
2026-01-07 16:08:32 +0100

    Second stage

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-enum Commands {
-    Unknown,
-}
+// enum Commands {
+//     Unknown,
+// }
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    let mut command = String::new();
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut command).unwrap();
-    
-    match command.trim() {
-        _ => {
-            println!("{}: command not found", command.trim());
+    loop {
+
+        let mut input = String::new();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let Some(command) = input.split_whitespace().next() else {
+            continue;
+        };
+
+        match command.trim() {
+            _ => {
+                println!("{}: command not found", command.trim());
+            }
         }
     }
+
 }
